e473203a306117912c5817abf981ee46a861be73
adieu <adieu@adieu.cn>
2012-05-05 09:46:45 +0800

    Added long description.

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 from setuptools import setup, find_packages
 
 
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(
     name = "xunlei",
-    version = "0.1.1",
+    version = "0.1.2",
     license = 'BSD',
     description = "Basic API for Xunlei Lixian",
+    long_description=long_description,
     author='Ivan Diao',
     author_email='adieu@adieu.me',
     url='http://github.com/adieu/xunlei',
