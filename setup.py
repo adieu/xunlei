9897badcf0c41c27bd498ea9717701900a9a0114
adieu <adieu@adieu.cn>
2012-05-05 09:42:13 +0800

    Added more info to setup.py.

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    import ez_setup
-    ez_setup.use_setuptools()
-    from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 
 setup(
     name = "xunlei",
     version = "0.1",
     license = 'BSD',
-    description = "Xunlei project provides basic api for xunlei lixian",
+    description = "Basic API for Xunlei Lixian",
+    author='Ivan Diao',
+    author_email='adieu@adieu.me',
+    url='http://github.com/adieu/xunlei',
     py_modules = ['xunlei'],
     scripts = ['xunlei_cli'],
     install_requires = ['BeautifulSoup', 'mechanize'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        "Topic :: Communications :: File Sharing",
+    ],
 )
